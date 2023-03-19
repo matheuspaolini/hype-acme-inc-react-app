99be993a798473552fa484e0bae5b8d30ca28eb8
Matheus <dev.paolini.matheus@gmail.com>
2023-03-18 22:12:40 -0300

    feat: react-router-dom router component setup

---
+++ src/main/routes/Router.tsx
@@ -0,0 +1,25 @@
+import { RootLayout } from 'presentation/layouts/RootLayout';
+import { CartPage } from 'presentation/pages/Cart';
+import { HomePage } from 'presentation/pages/Home';
+import { ProductPage } from 'presentation/pages/Product';
+import { createBrowserRouter } from 'react-router-dom';
+
+export const browserRouter = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      },
+      {
+        path: 'product/:productId',
+        element: <ProductPage />
+      },
+      {
+        path: 'cart',
+        element: <CartPage />
+      }
+    ]
+  }
+]);
