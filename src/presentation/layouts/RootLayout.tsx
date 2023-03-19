88a451294a97c754e2eb39bc02699e08c6138894
Matheus <dev.paolini.matheus@gmail.com>
2023-03-18 23:49:12 -0300

    feat: added bottom screen price preview component into root layout

--- src/presentation/layouts/RootLayout.tsx
+++ src/presentation/layouts/RootLayout.tsx
@@ -1,22 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 
 import { Footer } from 'presentation/layouts/Footer';
 import { Header } from 'presentation/layouts/Header';
 import { styled, useStitches } from 'presentation/styles/stitches.config';
+import { BottomScreenPricePreview } from 'presentation/layouts/BottomScreenPricePreview';
 
 import { makeLocalCreateProductList } from 'main/factories/use-cases/local-create-product-list-factory';
 
-import { GetScrollRestorationKeyFunction, Outlet, ScrollRestoration } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 
 const Container = styled('div', {
   width: '100%',
   height: '100%',
 });
-
-const paths = ['/'];
-
-const getKey: GetScrollRestorationKeyFunction = (location, matches) =>
-  paths.includes(location.pathname) ? location.pathname : location.key;
 
 export const localProductList = makeLocalCreateProductList().perform();
 
@@ -31,7 +27,17 @@ export function RootLayout() {
         <Footer />
       </Container>
 
-      <ScrollRestoration getKey={getKey} />
+      <BottomScreenPricePreview />
+
+      <ScrollRestoration
+        getKey={(location, matches) => {
+          const paths = ['/'];
+
+          return paths.includes(location.pathname)
+            ? location.pathname
+            : location.key
+        }}
+      />
     </Fragment>
   );
 }
